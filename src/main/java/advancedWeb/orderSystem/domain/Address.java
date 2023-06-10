3c714fe8728cd1dc31b0c21404cca8f046af6e4e
지성 최 <jisung7278@naver.com>
2023-06-10 15:41:25 +0900

    Address에 주석 추가 Delivery에 필드 추가 OrderStatus Class name을 DeliveryStatus로 변경 Order에 Getter, Setter, Entity 어노테이션 추가 및 id 추가

--- src/main/java/advancedWeb/orderSystem/domain/Address.java
+++ src/main/java/advancedWeb/orderSystem/domain/Address.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Embeddable
+//주문 시스템 요구 사항 중 가게 정보에 주소가 포함 됨
 public class Address {
     private String city;
     private String street;
--- src/main/java/advancedWeb/orderSystem/domain/Order.java
+++ src/main/java/advancedWeb/orderSystem/domain/Order.java
@@ -1,4 +1,19 @@
 package advancedWeb.orderSystem.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
 public class Order {
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
 }
