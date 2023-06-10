b4fad4fb5e39edddbba11f948a00ea011c6698b2
지성 최 <jisung7278@naver.com>
2023-06-10 15:31:57 +0900

    Menu 통합 및 변수 추가 할인 정책을 위한 Discount Entity 생성

---
+++ src/main/java/advancedWeb/orderSystem/domain/Discount.java
@@ -0,0 +1,25 @@
+package advancedWeb.orderSystem.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+public class Discount {
+    @Id
+    @GeneratedValue
+    @Column(name = "DISCOUNT_ID")
+    private Long id;
+
+    // 할인 금액이 비율로 적용 되는지 판별하기 위해 (정률 할인 시 %로 할인)
+    private boolean isRate;
+
+    // 할인 금액 혹은 비율
+    private int discountPrice;
+}
