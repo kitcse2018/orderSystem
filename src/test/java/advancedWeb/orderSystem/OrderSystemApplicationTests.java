137fb2a8126a50a0156f35d4dfb1792620a9a234
jiseong7278 <jisung7278@naver.com>
2023-06-13 10:50:18 +0900

    Menu Method Test Complete

--- src/test/java/advancedWeb/orderSystem/OrderSystemApplicationTests.java
+++ src/test/java/advancedWeb/orderSystem/OrderSystemApplicationTests.java
@@ -1,13 +1,15 @@
 package advancedWeb.orderSystem;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OrderSystemApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSystemApplication.class, args);
 	}
 
 }
